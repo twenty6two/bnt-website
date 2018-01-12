b5e6198b606a6429320be88f7b11f2f433fbf5fa
Christopher Nolden <christopher.nolden@gmail.com>
2018-01-11 23:32:39 -0800

    Implementing transitions for mobile collapsed secondary navigation

--- docroot/themes/custom/big_noise/js/BigNoise.js
+++ docroot/themes/custom/big_noise/js/BigNoise.js
@@ -3,6 +3,27 @@
     window.BigNoise = {};
   };
 
+  BigNoise.settings = {
+    breakpoints: {
+      mobile: {
+        min: 320,
+        max: 767
+      },
+      tablet: {
+        min: 768,
+        max: 1023
+      },
+      desktop: {
+        min: 1024,
+        max: 1299
+      },
+      desktopWide: {
+        min: 1300,
+        max: null
+      }
+    }
+  };
+
   BigNoise.activateSidebar = function (sidebarId) {
     var sidebarElement = $('#' + sidebarId);
 
@@ -50,5 +71,11 @@
     $('#site--main--secondary-navigation-toggle', context).click(function () {
       BigNoise.secondaryNavigationToggleClickHandler();
     });
+
+    document.querySelector('style').textContent += '\n' +
+      '@media (max-width: ' + this.settings.breakpoints.mobile.max + 'px) {\n' +
+        '.site--main--secondary-navigation-container.theme-collapsible-layout-mobile.expanded {\n' +
+          'height: ' + ($('#site--main--secondary-navigation-container').height() + $('#navigation--secondary-navigation-main').height()) + 'px;\n' +
+      '}\n';
   };
 })(jQuery);
